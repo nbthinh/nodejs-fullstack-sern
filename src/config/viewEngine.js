7cd863529609977ae3c96c2a92ea093b7ef2db9a
nbthinh <nguyenbuuthinh036@gmail.com>
2022-09-26 09:10:19 +0700

    first commit

---
+++ src/config/viewEngine.js
@@ -0,0 +1,8 @@
+import express from "express"
+let configViewEngine = (app) => {
+    // arrow functionfuufunctii
+    app.use(express.static("./src/public"));
+    app.set("view engine", "ejs");
+    app.set("views", "./src/views");
+}
+module.exports = configViewEngine;
