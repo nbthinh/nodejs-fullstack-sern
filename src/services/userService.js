c8f390d4043e7df71b6df47ca0366f1f6a2eda24
nbthinh <nguyenbuuthinh036@gmail.com>
2022-10-17 10:08:40 +0700

    #41 finish, message => errMessage

--- src/services/userService.js
+++ src/services/userService.js
@@ -116,24 +116,27 @@ let createNewUser = (data) => {
             if (check) {
                 resolve({
                     errCode: 1,
-                    message: "Your email is already in used"
+                    errMessage: "Your email is already in used"
                 });
             }
-            let hashPasswordFromBcrypt = await hashUserPassword(data.password);
-            await db.User.create({
-                email: data.email,
-                password: hashPasswordFromBcrypt,
-                firstName: data.firstName,
-                lastName: data.lastName,
-                address: data.address,
-                phoneNumber: data.phoneNumber,
-                gender: data.gender === '1' ? true : false,
-                roleId: data.roleId
-            });
-            resolve({
-                errCode: 0,
-                message: "OK" 
-            });
+            else {
+                let hashPasswordFromBcrypt = await hashUserPassword(data.password);
+                await db.User.create({
+                    email: data.email,
+                    password: hashPasswordFromBcrypt,
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    address: data.address,
+                    phoneNumber: data.phoneNumber,
+                    gender: data.gender === '1' ? true : false,
+                    roleId: data.roleId
+                });
+                resolve({
+                    errCode: 0,
+                    message: "OK" 
+                });
+            }
+            
         }catch(e){
             reject(e);
         }
